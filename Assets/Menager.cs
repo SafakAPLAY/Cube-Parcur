e0249923b7f56c97dbee68b5c15e45f7fc4399fb
Şafak Aplay <aplay.safak@gmail.com>
2020-08-07 00:07:46 +0300

    movement is added

---
+++ Assets/Menager.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Menager : MonoBehaviour
+{
+    public static Menager instance;
+    protected string classDefinition;
+    protected int level=1;
+    private void Awake()
+    {
+        MakeSingleton();
+    }
+    public void MakeSingleton()
+    {
+        if (instance != null)
+        {
+            Destroy(gameObject);
+        }
+        else
+        {
+            instance  = this;
+            DontDestroyOnLoad(gameObject);
+        }
+    }
+    // Start is called before the first frame update
+    void Start()
+    {
+        classDefinition = "Menager";
+        ShowClassDefinion();
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    protected void ShowLevel()
+    {
+        Debug.Log("Your level is ="+level.ToString());
+    }
+    protected void ShowClassDefinion()
+    {
+        Debug.Log("Class " + classDefinition + " is active");
+    }
+}
---
+++ Assets/PlayerMenager.cs
@@ -0,0 +1,51 @@
+﻿using UnityEngine;
+
+
+public class PlayerMenager : MonoBehaviour
+{
+    float secondX;
+    float secondy;
+    float secondz;
+
+    Vector3 up = new Vector3(0f, 0f, 1.0f);
+    Vector3 down = new Vector3(0f, 0f, -1.0f);
+    Vector3 rigth = new Vector3(1.0f, 0f, 0f);
+    Vector3 left = new Vector3(-1.0f, 0f, 0f);
+    Vector3 startPos;
+    public Transform transformTarget;
+    private void Start()
+    {
+        transformTarget = GameObject.Find("Player").transform;
+        startPos = GameObject.Find("Player").transform.position;
+    }
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.UpArrow))
+        {
+            Debug.Log("KeyCode.UpArrow was pressed");
+            transformTarget.position = transformTarget.position+up;
+
+        }
+        if (Input.GetKeyDown(KeyCode.DownArrow))
+        {
+            Debug.Log("KeyCode.DownArrow was pressed");
+            transformTarget.position = transformTarget.position + down;
+        }
+        if (Input.GetKeyDown(KeyCode.LeftArrow))
+        {
+            Debug.Log("KeyCode.LeftArrow was pressed");
+            transformTarget.position = transformTarget.position + left;
+        }
+        if (Input.GetKeyDown(KeyCode.RightArrow))
+        {
+            Debug.Log("KeyCode.RightArrow was pressed");
+            transformTarget.position = transformTarget.position + rigth;
+        }
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            Debug.Log("respawned");
+            transformTarget.transform.position = startPos;
+        }
+
+    }
+}
